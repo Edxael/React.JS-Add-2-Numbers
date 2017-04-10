a3dd2d1ad9469831227c4621810123861a76f636
Edxael <zerphyr.r@gmail.com>
2017-04-09 21:05:49 -0600

    First Commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM 'react-dom';
+import Math1 from './sum.jsx';
+
+ReactDOM.render(<Math1 />, document.getElementById("root"));
---
+++ src/sum.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './style.css';
+
+
+class Math1 extends Component{
+    render(){
+        return(
+            <p>Leaones</p>
+        )
+    }
+}
+
+export default Math1;
