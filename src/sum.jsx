2915aadfa961407b374f9e788d05ed5de5a3d7b9
Edxael <zerphyr.r@gmail.com>
2017-04-10 00:47:37 -0600

    finish app

--- src/sum.jsx
+++ src/sum.jsx
@@ -8,9 +8,18 @@ import './style.css';
 class Math1 extends Component{
 
     sum(){
-        var data1 = parseInt(document.getElementById(""));
-        var data2 = parseInt(document.getElementById(""));
+        var data1 = parseInt(document.getElementById("in1").value);
+        var data2 = parseInt(document.getElementById("in2").value);
         var total = data1 + data2;
+
+        if (isNaN(total)){
+            if(isNaN(data1) && Number.isInteger(data2)){console.log("First Empty"); total = data2;}
+            if(isNaN(data2) && Number.isInteger(data1)){console.log("Second Empty"); total = data1;}
+            if(isNaN(data1) && isNaN(data2)){console.log("Both Empty"); total = 0;}
+            console.log(" ");
+        }
+
+        document.getElementById("in3").innerHTML = total;
     }
 
     render(){
@@ -19,11 +28,37 @@ class Math1 extends Component{
 
                 <div className="app-cont">
                     <h1>Add Two Numbers</h1>
-                    <p>Powered by React.js</p>
+                    <p className="sub-t">Powered by React.js</p>
+
+                    <img className="pic1" src={Pict1} alt="Y la Pic??" />
+
+                    <div className="data-cont">
+
+                        <div className="indata">
+                            <label>Number 1:</label>
+                            <input type="text" name="perc" id="in1" className="in1" />
+                        </div><br/>
+
+
+                        <div className="indata">
+                            <label>Number 2:</label>
+                            <input type="text" name="perc" id="in2" className="in1" />
+                        </div><br/>
+
+
+                        <input className="button1" type="submit" onClick={() => this.sum()} value="Sum Numbers"></input><br/><br/>
+
+
+                        <div className="indata">
+                            <label>-- Total --</label>
+                            <div id="in3" className="answ1"></div>
+                        </div><br/>
+
+                    </div>
 
 
                 </div>
-                
+
                 <div className="bottom1 trans">
                     <div className="name"> <p>by: Edmundo Rubio</p> </div>
                     <div><a href="https://github.com/Edxael" target="blank"> <img className="icon" src={logo1} alt="??" /></a></div>
