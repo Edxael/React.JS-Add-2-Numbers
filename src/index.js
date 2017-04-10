cfffbc27c2dbe90ad8c343b963a1cee097e80e9d
Edxael <zerphyr.r@gmail.com>
2017-04-09 21:40:20 -0600

    Updat 1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM 'react-dom';
+import ReactDOM from 'react-dom';
 import Math1 from './sum.jsx';
 
 ReactDOM.render(<Math1 />, document.getElementById("root"));
--- src/sum.jsx
+++ src/sum.jsx
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
+import Pict1 from './IMG/up1.png';
+import logo1 from './IMG/git3.png';
+import logo2 from './IMG/pf1.png';
 import './style.css';
 
 
 class Math1 extends Component{
+
+    sum(){
+        var data1 = parseInt(document.getElementById(""));
+        var data2 = parseInt(document.getElementById(""));
+        var total = data1 + data2;
+    }
+
     render(){
         return(
-            <p>Leaones</p>
+            <div className="m-cont">
+
+                <div className="app-cont">
+                    <h1>Add Two Numbers</h1>
+                    <p>Powered by React.js</p>
+
+
+                </div>
+                
+                <div className="bottom1 trans">
+                    <div className="name"> <p>by: Edmundo Rubio</p> </div>
+                    <div><a href="https://github.com/Edxael" target="blank"> <img className="icon" src={logo1} alt="??" /></a></div>
+                    <div><a href="http://www.mycode.website/" target="blank"> <img className="icon" src={logo2} alt="??" /></a></div>
+                </div>
+
+            </div>
         )
     }
 }
